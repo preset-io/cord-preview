950c5813d51d5766897a35d1c92dedbbf586d99a
Daniel Gaspar <danielvazgaspar@gmail.com>
2024-07-17 21:50:53 +0100

    test

--- external/src/entrypoints/console/const.ts
+++ external/src/entrypoints/console/const.ts
@@ -1,4 +1,4 @@
-export const AUTH0_AUDIENCE = 'http://console-cord.app-sdx.preset.io';
+export const AUTH0_AUDIENCE = 'https://console-cord.app-sdx.preset.io';
 export const DRAWER_WIDTH = 256;
 export const TOOLBAR_HEIGHT = 68;
 export const DEFAULT_EMAIL_DOMAIN = 'cord.fyi';
